96c692bdb06a494752ee3ccd5b63dc63fdabd933
kiv1n <aloha@kiv1n.ru>
2025-01-15 17:37:26 +0300

    Add TODOs in the bot project

--- bot/scripts/lib/bot.ts
+++ bot/scripts/lib/bot.ts
@@ -22,6 +22,7 @@ export default class Bot {
         // Save the chat in the database
         insertChat({ id: chatId.toString(), createdTime: new Date() });
         // Send an info message
+        // TODO: Add link to the web app
         bot.sendMessage(
           chatId,
           "As soon as the AI agent finds a cool idea to launch a token, you'll get a notification 🔔"
